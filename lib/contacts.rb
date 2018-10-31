7ba9f5f6f82df1a7e10c93618120cef6b43dfa2a
Johnathon Wood <jaywood128@gmail.com>
2018-10-31 22:00:06 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,9 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do|attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_scream|
+          if attribute ==
+      :favorite_ice_cream_flavors
+            data.delete_if {|ice_scream|
       ice_cream == "strawberry"}
   end
 end
